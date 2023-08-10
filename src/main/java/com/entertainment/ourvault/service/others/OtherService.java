317d5163fe44cf284fea05b3a798ff727cedcb97
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 13:49:10 -0500

    Addying the methods to use into the OtherService for Category classes

--- src/main/java/com/entertainment/ourvault/service/others/OtherService.java
+++ src/main/java/com/entertainment/ourvault/service/others/OtherService.java
@@ -1,5 +1,6 @@
 package com.entertainment.ourvault.service.others;
 
+import com.entertainment.ourvault.model.dto.CategoryDto;
 import com.entertainment.ourvault.model.dto.TypeDto;
 
 import java.util.List;
@@ -16,4 +17,42 @@ public interface OtherService {
     TypeDto saveType(TypeDto typeDto);
 
     boolean deleteTypeById(int idType);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    List<CategoryDto> getAllCategories();
+
+    Optional<CategoryDto> findCategoryById(int idCategory);
+
+    Optional<CategoryDto> findCategoryByName(String name);
+
+    CategoryDto saveCategory(CategoryDto categoryDto);
+
+    boolean deleteCategoryById(int idCategory);
 }
