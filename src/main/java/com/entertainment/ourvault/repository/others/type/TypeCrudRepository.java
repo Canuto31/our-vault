ed4ca03203a08ec6eb161a4d48d113c8a61fa097
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 08:34:37 -0500

    Creation of the TypeCrudRepository

---
+++ src/main/java/com/entertainment/ourvault/repository/others/type/TypeCrudRepository.java
@@ -0,0 +1,9 @@
+package com.entertainment.ourvault.repository.others.type;
+
+import com.entertainment.ourvault.model.entity.Type;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TypeCrudRepository extends CrudRepository<Type, Integer> {
+
+
+}
