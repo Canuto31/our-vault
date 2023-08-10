8c8e6cb71bed61929407f19310fb5e8145cc2656
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 13:36:43 -0500

    Addying the logic into the CategoryMapper

--- src/main/java/com/entertainment/ourvault/mapper/CategoryMapper.java
+++ src/main/java/com/entertainment/ourvault/mapper/CategoryMapper.java
@@ -4,27 +4,50 @@ import com.entertainment.ourvault.model.dto.CategoryDto;
 import com.entertainment.ourvault.model.entity.Category;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class CategoryMapper implements BaseMapper<CategoryDto, Category> {
     @Override
     public CategoryDto entityToDto(Category entity) {
-        return null;
+        CategoryDto dto = new CategoryDto();
+
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+
+        return dto;
     }
 
     @Override
     public Category dtoToEntity(CategoryDto dto) {
-        return null;
+        Category entity = new Category();
+
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+
+        return entity;
     }
 
     @Override
     public List<CategoryDto> entitiesToDtos(List<Category> entities) {
-        return null;
+        List<CategoryDto> dtos = new ArrayList<>();
+
+        for(Category entity : entities) {
+            dtos.add(entityToDto(entity));
+        }
+
+        return dtos;
     }
 
     @Override
     public List<Category> DtosToEntities(List<CategoryDto> dtos) {
-        return null;
+        List<Category> entities = new ArrayList<>();
+
+        for(CategoryDto dto : dtos) {
+            entities.add(dtoToEntity(dto));
+        }
+
+        return entities;
     }
 }
