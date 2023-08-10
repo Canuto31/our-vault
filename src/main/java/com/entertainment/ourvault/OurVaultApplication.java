1e8caeea735b04de1a6ae69ca109a702fcaba8ab
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-09 22:24:07 -0500

    first commit

---
+++ src/main/java/com/entertainment/ourvault/OurVaultApplication.java
@@ -0,0 +1,13 @@
+package com.entertainment.ourvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entertainment/ourvault/OurVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entertainment.ourvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
