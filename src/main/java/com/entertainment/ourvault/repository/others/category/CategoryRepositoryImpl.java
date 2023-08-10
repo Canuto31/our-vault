df77922ef71305ff65cb2080fe415f902249fcab
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 13:46:18 -0500

    Creation of the logic into the CategoryRepositoryImpl

--- src/main/java/com/entertainment/ourvault/repository/others/category/CategoryRepositoryImpl.java
+++ src/main/java/com/entertainment/ourvault/repository/others/category/CategoryRepositoryImpl.java
@@ -1,6 +1,9 @@
 package com.entertainment.ourvault.repository.others.category;
 
+import com.entertainment.ourvault.mapper.CategoryMapper;
 import com.entertainment.ourvault.model.dto.CategoryDto;
+import com.entertainment.ourvault.model.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,28 +11,36 @@ import java.util.Optional;
 
 @Repository
 public class CategoryRepositoryImpl implements CategoryRepository{
+
+    @Autowired
+    private CategoryCrudRepository repository;
+    @Autowired
+    private CategoryMapper mapper;
+
     @Override
     public List<CategoryDto> getAllCategories() {
-        return null;
+        List<Category> categories = (List<Category>) repository.findAll();
+        return mapper.entitiesToDtos(categories);
     }
 
     @Override
     public Optional<CategoryDto> GetCategoryById(int idCategory) {
-        return Optional.empty();
+        return repository.findById(idCategory).map(category -> mapper.entityToDto(category));
     }
 
     @Override
     public Optional<CategoryDto> getCategoryByName(String name) {
-        return Optional.empty();
+        return repository.findCategoryByName(name).map(category -> mapper.entityToDto(category));
     }
 
     @Override
     public CategoryDto saveCategory(CategoryDto categoryDto) {
-        return null;
+        Category category = mapper.dtoToEntity(categoryDto);
+        return mapper.entityToDto(repository.save(category));
     }
 
     @Override
     public void deleteCategory(int idCategory) {
-
+        repository.deleteById(idCategory);
     }
 }
