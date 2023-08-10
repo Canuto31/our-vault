4248f52a6db8d84c62ad306b690e7a1c635dc834
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 18:59:47 -0500

    Modifying the dtos for add the ManyToMany relation, between Type and State

--- src/main/java/com/entertainment/ourvault/model/dto/StateDto.java
+++ src/main/java/com/entertainment/ourvault/model/dto/StateDto.java
@@ -2,9 +2,12 @@ package com.entertainment.ourvault.model.dto;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class StateDto {
 
     private Integer id;
     private String name;
+    private Set<String> types;
 }
--- src/main/java/com/entertainment/ourvault/model/dto/TypeDto.java
+++ src/main/java/com/entertainment/ourvault/model/dto/TypeDto.java
@@ -2,9 +2,12 @@ package com.entertainment.ourvault.model.dto;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class TypeDto {
 
     private Integer id;
     private String name;
+    private Set<String> states;
 }
