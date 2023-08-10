291fec623e6860868e627457c7dde5c871342caf
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 09:02:26 -0500

    Implementation of the methods into the TypeRepositoryImpl

--- src/main/java/com/entertainment/ourvault/repository/others/type/TypeRepositoryImpl.java
+++ src/main/java/com/entertainment/ourvault/repository/others/type/TypeRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.entertainment.ourvault.repository.others.type;
 
+import com.entertainment.ourvault.mapper.TypeMapper;
 import com.entertainment.ourvault.model.dto.TypeDto;
+import com.entertainment.ourvault.model.entity.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -12,29 +14,33 @@ public class TypeRepositoryImpl implements TypeRepository{
 
     @Autowired
     private TypeCrudRepository repository;
+    @Autowired
+    private TypeMapper mapper;
 
     @Override
     public List<TypeDto> getAll() {
-        return null;
+        List<Type> types = (List<Type>) repository.findAll();
+        return mapper.entitiesToDtos(types);
     }
 
     @Override
     public Optional<TypeDto> GetTypeById(int idType) {
-        return Optional.empty();
+        return repository.findById(idType).map(type -> mapper.entityToDto(type));
     }
 
     @Override
     public Optional<TypeDto> getTypeByName(String typeName) {
-        return Optional.empty();
+        return repository.findByName(typeName).map(type -> mapper.entityToDto(type));
     }
 
     @Override
     public TypeDto saveType(TypeDto typeDto) {
-        return null;
+        Type type = mapper.dtoToEntity(typeDto);
+        return mapper.entityToDto(repository.save(type));
     }
 
     @Override
     public void delete(int idType) {
-
+        repository.deleteById(idType);
     }
 }
