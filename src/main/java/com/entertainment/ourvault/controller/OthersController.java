1f91b5b04bc939badfbfc9ee1eae302cf087bf33
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 09:41:52 -0500

    Add the method for the get type by name mapping into the OthersController

--- src/main/java/com/entertainment/ourvault/controller/OthersController.java
+++ src/main/java/com/entertainment/ourvault/controller/OthersController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,10 @@ public class OthersController {
     @GetMapping("/type")
     private ResponseEntity<List<TypeDto>> getAll() {
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/type/name/{typeName}")
+    private ResponseEntity<TypeDto> getByTypeName(@PathVariable String typeName) {
+        return service.findByName(typeName).map(type -> new ResponseEntity<>(type, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
