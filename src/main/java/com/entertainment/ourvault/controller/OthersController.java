b4b04225e3165f5dcfe07131b781014f5c61a131
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 09:44:46 -0500

    Add the method for the save type mapping into the OthersController

--- src/main/java/com/entertainment/ourvault/controller/OthersController.java
+++ src/main/java/com/entertainment/ourvault/controller/OthersController.java
@@ -5,10 +5,7 @@ import com.entertainment.ourvault.service.others.OtherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +34,10 @@ public class OthersController {
     @GetMapping("/type/name/{typeName}")
     private ResponseEntity<TypeDto> getByTypeName(@PathVariable String typeName) {
         return service.findByName(typeName).map(type -> new ResponseEntity<>(type, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/type")
+    private ResponseEntity<TypeDto> saveType(@RequestBody TypeDto typeDto) {
+        return new ResponseEntity<>(service.saveType(typeDto), HttpStatus.CREATED);
     }
 }
