4f1b9315fe983bdfb43b5c1972cd1209b5f309e9
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-09 22:47:38 -0500

    Add of the base mapper for use into the all mappers

---
+++ src/main/java/com/entertainment/ourvault/mapper/BaseMapper.java
@@ -0,0 +1,14 @@
+package com.entertainment.ourvault.mapper;
+
+import java.util.List;
+
+public interface BaseMapper<D, E> {
+
+    public D entityToDto(E entity);
+
+    public E dtoToEntity(D dto);
+
+    public List<D> entitiesToDtos(List<E> entities);
+
+    public List<E> DtosToEntities(List<D> dtos);
+}
