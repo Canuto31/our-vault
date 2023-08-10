079ce751f920fb3df0e871acf0f5f00112108777
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-10 09:37:15 -0500

    Add the method for the get all types mapping into the OthersController

--- src/main/java/com/entertainment/ourvault/controller/OthersController.java
+++ src/main/java/com/entertainment/ourvault/controller/OthersController.java
@@ -1,15 +1,30 @@
 package com.entertainment.ourvault.controller;
 
+import com.entertainment.ourvault.model.dto.TypeDto;
+import com.entertainment.ourvault.service.others.OtherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/others")
 public class OthersController {
 
+    @Autowired
+    private OtherService service;
+
     @GetMapping("/say-hi")
     private String sayHello() {
         return "Hello world";
+    }
+
+    @GetMapping("/type")
+    private ResponseEntity<List<TypeDto>> getAll() {
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 }
