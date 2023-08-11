34e3903a5a6daea25de04b7d76e2333d89434b4a
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-11 09:52:10 -0500

    Creating the Book Mapper without relationship

---
+++ src/main/java/com/entertainment/ourvault/mapper/BookMapper.java
@@ -0,0 +1,49 @@
+package com.entertainment.ourvault.mapper;
+
+import com.entertainment.ourvault.mapper.utils.BaseMapper;
+import com.entertainment.ourvault.mapper.utils.MapperUtils;
+import com.entertainment.ourvault.model.dto.BookDto;
+import com.entertainment.ourvault.model.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BookMapper implements BaseMapper<BookDto, Book> {
+
+    @Lazy
+    @Autowired
+    private MapperUtils mapperUtils;
+
+    @Override
+    public BookDto entityToDto(Book entity) {
+        BookDto dto = new BookDto();
+
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+
+        return dto;
+    }
+
+    @Override
+    public Book dtoToEntity(BookDto dto) {
+        Book entity = new Book();
+
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+
+        return entity;
+    }
+
+    @Override
+    public List<BookDto> entitiesToDtos(List<Book> entities) {
+        return mapperUtils.entitiesToDtos(entities, this::entityToDto);
+    }
+
+    @Override
+    public List<Book> DtosToEntities(List<BookDto> dtos) {
+        return mapperUtils.dtosToEntities(dtos, this::dtoToEntity);
+    }
+}
