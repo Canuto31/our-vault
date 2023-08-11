4af0e2294daeed9a14e771c29669b59a7a6d2aba
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-11 17:50:00 -0500

    Creation of the GameStoreRepository interface

---
+++ src/main/java/com/entertainment/ourvault/repository/others/gameStore/GameStoreRepository.java
@@ -0,0 +1,19 @@
+package com.entertainment.ourvault.repository.others.gameStore;
+
+import com.entertainment.ourvault.model.dto.GameStoreDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GameStoreRepository {
+
+    List<GameStoreDto> getAllGameStores();
+
+    Optional<GameStoreDto> GetGameStroreById(int idGameStore);
+
+    Optional<GameStoreDto> getGameStoreByName(String name);
+
+    GameStoreDto saveGameStore(GameStoreDto gameStoreDto);
+
+    void deleteGameStore(int idBook);
+}
