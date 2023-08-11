73e8202202f6bd64ce9e9eedc3a0771fe93c4d8a
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-11 09:03:30 -0500

    Creation of the AuthorRepository implementation with logic into the methods

---
+++ src/main/java/com/entertainment/ourvault/repository/others/author/AuthorRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.entertainment.ourvault.repository.others.author;
+
+import com.entertainment.ourvault.mapper.AuthorMapper;
+import com.entertainment.ourvault.model.dto.AuthorDto;
+import com.entertainment.ourvault.model.entity.Author;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class AuthorRepositoryImpl implements AuthorRepository{
+
+    @Autowired
+    private AuthorCrudRepository repository;
+    @Autowired
+    private AuthorMapper mapper;
+
+    @Override
+    public List<AuthorDto> getAllAuthor() {
+        List<Author> authors = (List<Author>) repository.findAll();
+        return mapper.entitiesToDtos(authors);
+    }
+
+    @Override
+    public Optional<AuthorDto> GetAuthorById(int idAuthor) {
+        return repository.findById(idAuthor).map(author -> mapper.entityToDto(author));
+    }
+
+    @Override
+    public Optional<AuthorDto> getAuthorByName(String name) {
+        return repository.findAuthorByName(name).map(author -> mapper.entityToDto(author));
+    }
+
+    @Override
+    public AuthorDto saveAuthor(AuthorDto authorDto) {
+        Author author = mapper.dtoToEntity(authorDto);
+        return mapper.entityToDto(repository.save(author));
+    }
+
+    @Override
+    public void deleteAuthor(int idAuthor) {
+        repository.deleteById(idAuthor);
+    }
+}
